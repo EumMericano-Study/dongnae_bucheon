513867eaaa83dc499281400f964f4f17f82cd66e
Eum Young Hyun <opwer032@naver.com>
2021-12-11 02:26:12 +0900

    feat: Init component

---
+++ bucheon/src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+function App() {
+  return <Container></Container>;
+}
+
+export default App;
+
+const Container = styled.div``;
