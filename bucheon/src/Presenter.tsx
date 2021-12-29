f81cf7df0443e8963b2fae5c0b0e47c462960f07
Eum <opwer032@naver.com>
2021-12-29 11:46:12 +0900

    test: Test react-helmet

--- bucheon/src/Presenter.tsx
+++ bucheon/src/Presenter.tsx
@@ -1,73 +1,74 @@
 import React from "react";
 import styled from "styled-components";
 import Intro from "./components/intro";
-
-import green from "./assets/green.jpg";
-import stairs from "./assets/stairs.jpg";
+import { Helmet } from "react-helmet";
 
 function Presenter() {
-  return (
-    <Container>
-      <Intro />
-      <StickyCont>
-        <Sticky>
-          <Img src={green} />
-          <Content>한국에서 가장 따스한 저희 동네를 소개합니다</Content>
-        </Sticky>
-      </StickyCont>
-      <StickyCont>
-        <Sticky>
-          <Content>가나다라</Content>
-          <Img src={stairs} />
-        </Sticky>
-      </StickyCont>
-      <Block />
-    </Container>
-  );
+    return (
+        <Container>
+            <Helmet>
+                <title>react-helmet test</title>
+            </Helmet>
+            <Intro />
+            <StickyCont>
+                <Sticky>
+                    <Content>
+                        한국에서 가장 따스한 저희 동네를 소개합니다
+                    </Content>
+                </Sticky>
+            </StickyCont>
+            <StickyCont>
+                <Sticky>
+                    <Content>가나다라</Content>
+                </Sticky>
+            </StickyCont>
+            <Block />
+        </Container>
+    );
 }
 
 export default Presenter;
 
 const Container = styled.div`
-  max-width: 100%;
-  min-height: 200vh;
+    max-width: 100%;
+    min-height: 200vh;
 `;
 
 const StickyCont = styled.section`
-  width: 100%;
-  height: 130vh;
+    width: 100%;
+    height: 130vh;
 
-  position: relative;
-  display: flex;
+    position: relative;
+    display: flex;
 `;
 
 const Sticky = styled.div`
-  width: 100%;
-  height: 100vh;
+    width: 100%;
+    height: 100vh;
 
-  position: sticky;
-  top: 0;
+    position: sticky;
+    top: 0;
 
-  display: flex;
+    display: flex;
 `;
 
 const Img = styled.img`
-  width: 50%;
-  height: 100%;
+    width: 50%;
+    height: 100%;
 `;
 
 const Content = styled.div`
-  width: 50%;
-  height: 100%;
+    width: 50%;
+    height: 100%;
 
-  display: flex;
-  justify-content: center;
-  align-items: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
-  font-size: 2rem;
+    font-size: 2rem;
 `;
 
 const Block = styled.div`
-  width: 100%;
-  height: 10vh;
+    width: 100%;
+    height: 10vh;
 `;
--- bucheon/src/components/intro.tsx
+++ bucheon/src/components/intro.tsx
@@ -1,24 +1,18 @@
 import React from "react";
 import styled from "styled-components";
 
-import intro from "../assets/intro.jpg";
-
 function Intro() {
-  return (
-    <Container>
-      <Background src={intro} />
-    </Container>
-  );
+    return <Container></Container>;
 }
 
 export default Intro;
 
 const Container = styled.section`
-  width: 100%;
-  height: 100vh;
+    width: 100%;
+    height: 100vh;
 `;
 
 const Background = styled.img`
-  width: 100%;
-  height: 100%;
+    width: 100%;
+    height: 100%;
 `;
